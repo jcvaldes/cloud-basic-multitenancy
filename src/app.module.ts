ab0b3e25806bfe1afa584d2b528edc0e9a9090c6
Juan Carlos Valdés <idevkingos@gmail.com>
2021-09-22 14:52:07 -0300

    configuracion de typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TenancyModule } from './tenancy/tenancy.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConnectionOptions } from 'typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TenancyModule,
+    // cargo variables de entorno cuando levanta la aplicacion
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      async useFactory(config: ConfigService) {
+        return {
+          type: 'mysql',
+          host: config.get('DB_HOST'),
+          username: config.get('DB_USER'),
+          password: config.get('DB_PASSWORD'),
+          port: +config.get('DB_PORT'),
+          database: config.get('DB_NAME'),
+          autoLoadEntities: true,
+          // solo en dev sinchronize: true asi cuando se actualiza el modelo impacta en la db
+          synchronize: true,
+          ssl: true,
+        } as ConnectionOptions;
+      },
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
